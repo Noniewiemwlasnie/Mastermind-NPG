e27dd7895650663bfe698e3a238972398f9799d4
Michal <michal.290305@onet.pl>
2025-05-12 16:48:40 +0200

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -41,14 +41,36 @@ import json
 # - Jeśli kodujący popełni błąd w ocenie, gra jest powtarzana, a odgadujący dostaje +3 punkty.
 
 
+#Losowanie liczb
+class Kolor:
+    def __init__(self):
+        self.liczba = random.randint(1, 6)
 
-def print_hi(name):
+    def get_kolor_slownie(self):
+        switch = {
+            1: "Czerwony",
+            2: "Niebieski",
+            3: "Zielony",
+            4: "Żółty",
+            5: "Fioletowy",
+            6: "Pomarańczowy"
+        }
+        return switch.get(self.liczba, "Nieznany kolor")
 
-    print(f'Hi, {name}')
+    def get_liczba(self):
+        return self.liczba
 
+miejsce1 = Kolor()
+miejsce2 = Kolor()
+miejsce3 = Kolor()
+miejsce4 = Kolor()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print("Wylosowana liczba:", miejsce1.get_liczba(),
+      miejsce2.get_liczba(),
+      miejsce3.get_liczba(),
+      miejsce4.get_liczba())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Kolor słownie:", miejsce1.get_kolor_slownie(),
+      miejsce2.get_kolor_slownie(),
+      miejsce3.get_kolor_slownie(),
+      miejsce4.get_kolor_slownie())
