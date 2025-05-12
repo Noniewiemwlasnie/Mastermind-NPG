41f4bd7d0efbbc3cc6d981a2f9c92f492a4d6c49
Michal <michal.290305@onet.pl>
2025-05-12 14:10:30 +0200

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -5,6 +5,43 @@ import csv
 import os
 import json
 
+#TODO: Funkcjonalności:
+#   Implementacja zasad gry.
+#   Gra z komputerem.
+#   Ustawienie poziomu trudności.
+#   Statystyka wygranych parametrów.
+#   Zapis/odczyt stanu gry.
+#   Jedna zaproponowane przez grupę.
+
+# Zasady gry
+
+# Elementy gry:
+# - Plansza z zasłoniętym rzędem na kod i 10 rzędami na próby.
+# - Duże pionki w 6 kolorach (do kodu i prób).
+# - Małe szpilki: białe i czarne (do oceny prób).
+
+# Zasady rozgrywki:
+# - Gra dla 2 osób; ustalają parzystą liczbę rund.
+# - Jeden gracz układa tajny kod z 4 pionków (mogą się powtarzać).
+# - Drugi gracz ma 10 prób na odgadnięcie kodu (kolory i kolejność).
+# - Po każdej próbie kodujący ocenia ją szpilkami:
+#   - Czarna – właściwy kolor i pozycja.
+#   - Biała – właściwy kolor, zła pozycja.
+
+# Zakończenie gry:
+# - Gra kończy się po trafnym odgadnięciu kodu lub po 10 próbach.
+# - Punktacja:
+#   - Kodujący: 1 punkt za każdą próbę przeciwnika + 1 za nierozwiązany kod.
+#   - Wygrywa gracz z większą liczbą punktów po ustalonej liczbie rund.
+
+# Alternatywna punktacja:
+# - Odgadujący: 1 punkt za każdą próbę; wygrywa gracz z mniejszą liczbą punktów.
+
+# Dodatkowa zasada:
+# - Jeśli kodujący popełni błąd w ocenie, gra jest powtarzana, a odgadujący dostaje +3 punkty.
+
+
+
 def print_hi(name):
 
     print(f'Hi, {name}')
