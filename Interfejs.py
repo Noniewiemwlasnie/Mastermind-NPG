4d53dda41afffc77ecd06e78e83c5be85ebaa572
Michal <michal.290305@onet.pl>
2025-05-22 11:57:20 +0200

    Merge remote-tracking branch 'origin/master'

--- Interfejs.py
+++ Interfejs.py
@@ -1,6 +1,38 @@
-from PySide6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit
+from PySide6.QtWidgets import QSizePolicy, QSpacerItem, QLabel, QDialog, QPushButton, QLineEdit, QScrollArea, QVBoxLayout, QWidget
 from PySide6.QtGui import Qt
 from Kolor import Kolor, sprawdz_kod
+
+# Menu
+class OptionsDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Menu")
+        self.setFixedSize(200, 150)
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(10, 10, 10, 10)  # Marginesy
+
+        # Etykieta w lewym górnym rogu
+        label = QLabel("Wybierz opcję:")
+        layout.addWidget(label, Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+
+        # Przycisk opcje
+        self.opcje_button = QPushButton("Ustawienia")
+        layout.addWidget(self.opcje_button)
+
+        # Przycisk statystyki
+        self.statystyki_button = QPushButton("Statystyki")
+        layout.addWidget(self.statystyki_button)
+
+        # Przycisk poziom trudności
+        self.poziom_trud_button = QPushButton("Poziom trudności")
+        layout.addWidget(self.poziom_trud_button)
+
+        # Spacer aby przyciski nie rozciągały się na całą wysokość
+        spacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        layout.addItem(spacer)
+
+        self.setLayout(layout)
 
 # Box kolorowy
 class ColorBox(QLineEdit):
@@ -42,6 +74,12 @@ class ColorBox(QLineEdit):
 
 # Główne okno
 class MyApp(QWidget):
+    #Do menu
+    def show_options_dialog(self):
+        dialog = OptionsDialog()
+        dialog.exec()
+
+    #Główna część "Mastermind"
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Gra mastermind")
@@ -57,31 +95,60 @@ class MyApp(QWidget):
             box = ColorBox(50 + i * 120, 50, self)
             self.boxes.append(box)
 
-        # Przycisk
-        self.submit_button = QPushButton("Submit", self)
-        self.submit_button.move(250, 180)
+        # Przycisk zatwierdz
+        self.submit_button = QPushButton("Zatwierdz", self)
+        self.submit_button.move(240, 180)
         self.submit_button.clicked.connect(self.sprawdz)
+
+        # Przycisk menu
+        self.menu_button = QPushButton("Menu", self)
+        self.menu_button.move(470, 10)
+        self.menu_button.clicked.connect(self.show_options_dialog)
 
         # Label z wynikiem
         self.result_label = QLabel("", self)
-        self.result_label.setGeometry(30, 250, 500, 60)
+        self.result_label.setGeometry(20, 250, 500, 30)
         self.result_label.setAlignment(Qt.AlignCenter)
         self.result_label.setStyleSheet("font-size: 24px;")
+
+        # Obszar historii wyników
+        self.history_scroll = QScrollArea(self)
+        self.history_scroll.setGeometry(20, 300, 520, 480)
+        self.history_scroll.setWidgetResizable(True)
+
+        self.history_widget = QWidget()
+        self.history_layout = QVBoxLayout(self.history_widget)
+        self.history_scroll.setWidget(self.history_widget)
+
+        # Inicjalizacja historii
+        self.history_entries = []
 
     def sprawdz(self):
         propozycja = [box.get_value() for box in self.boxes]
         wynik = sprawdz_kod(propozycja, self.secret_code)
 
-        # Tworzenie wyniku w stylu Wordle
-        wynik_ikony = ""
-        czarna = 0
-        biała = 0
-        for element in wynik:
-            if element == "czarna":
-                czarna += 1
-            elif element == "biała":
-                biała += 1
-        wynik_ikony += "Czarne: "+str(czarna)+"\nBiałe: "+str(biała)
-        self.result_label.setText(wynik_ikony)
-        if wynik.count("czarna") == 4:
-            self.result_label.setText("🎉 Zgadłeś! 🎉")
+        # Obliczanie wyniku
+        czarna = wynik.count("czarna")
+        biała = wynik.count("biała")
+
+        # Aktualizacja wyniku bieżącej próby
+        wynik_tekst = f"Czarne: {czarna}    Białe: {biała}"
+        self.result_label.setText(wynik_tekst)
+
+        # Dodanie do historii
+        history_label = QLabel(f"{len(self.history_entries) + 1}. Czarne: {czarna} Białe: {biała}")
+        history_label.setStyleSheet("font-size: 18px; margin: 5px;")
+        self.history_layout.addWidget(history_label)
+        self.history_entries.append(history_label)
+
+        # Przewiń do dołu
+        self.history_scroll.verticalScrollBar().setValue(
+            self.history_scroll.verticalScrollBar().maximum()
+        )
+
+        index = len(self.history_entries) #liczy index z przodu historii
+        if czarna == 4:
+            self.result_label.setText("😁Wygrałeś😁")
+            self.submit_button.setEnabled(False)
+        elif index >= 10:
+            self.result_label.setText("😭Przegrałeś😭")
