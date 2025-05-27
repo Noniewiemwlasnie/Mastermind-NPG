4ceadf8c1f61e577f60e578dbbfac47094ae76f7
Michal <michal.290305@onet.pl>
2025-05-27 12:45:57 +0200

    commity.csv

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="Mastermind",
+    install_requires=[
+        "PyQt6",
+        "PySide6",
+    ],
+)
