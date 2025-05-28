8f55cf2cb98fba4f0efa5e538558a1916e85e3d9
Kas <kacpersali@gmail.com>
2025-05-28 18:16:51 +0200

    box

--- interfejs.py
+++ interfejs.py
@@ -4,28 +4,6 @@ from losowanie_kolorów import Kolor, sprawdz_kod
 from menu import OptionsDialog
 from boxy_kolorowe import ColorBox
 
-#dodana opcja wyboru trudności połączona z ilością boxów
-class OptionsDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle("Opcje")
-
-        layout = QVBoxLayout(self)
-
-        self.difficulty_spinbox = QSpinBox()
-        self.difficulty_spinbox.setRange(4, 6)
-        self.difficulty_spinbox.setValue(4)
-        layout.addWidget(QLabel("Ilość pól do zgadnięcia: "))
-        layout.addWidget(self.difficulty_spinbox)
-
-        ok_button = QPushButton("OK")
-        ok_button.clicked.connect(self.accept)
-        layout.addWidget(ok_button)
-
-    def liczba_boxy(self):
-        return self.difficulty_spinbox.value()
-
-
 # Główne okno
 class MyApp(QWidget):
   
